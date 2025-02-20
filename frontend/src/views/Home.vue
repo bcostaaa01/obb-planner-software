4c908b045ef02e51bb265d960084a0fa6d292eb8
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-20 22:35:16 +0100

    feat: setup vue router

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
-    <div>
-        <h1>Home</h1>
-        <FwbButton>Click me</FwbButton>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1>{{ t('home.title') }}</h1>
     </div>
 </template>
 
 <script setup lang="ts">
-import { FwbButton } from 'flowbite-vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>
