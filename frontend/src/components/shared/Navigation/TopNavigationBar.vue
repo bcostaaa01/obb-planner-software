07c9fc6582625b8de893a14902806ba4f2b9e473
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-20 23:11:22 +0100

    feat: add top navigation bar and translations

---
+++ frontend/src/components/shared/Navigation/TopNavigationBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex top-0 z-50 w-full bg-red-900 py-4 pl-10">
+        <span class="text-white text-2xl font-bold">{{ t('nav-bar.title') }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="flex flex-col items-center justify-center h-screen">
-        <h1>{{ t('home.title') }}</h1>
+    <div class="flex flex-col h-screen">
+        <TopNavigationBar />
+        <div class="flex flex-col items-center justify-center h-screen">
+            
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
+import TopNavigationBar from '../components/shared/Navigation/TopNavigationBar.vue';
 
 const { t } = useI18n();
 </script>
