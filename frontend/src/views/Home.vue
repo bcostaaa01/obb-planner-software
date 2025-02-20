880b23909bafd4175cc35a8b21c53f12a1d6d24f
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-20 22:31:16 +0100

    feat: add locale files, clean up

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <FwbButton>Click me</FwbButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { FwbButton } from 'flowbite-vue';
+</script>
