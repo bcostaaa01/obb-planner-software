601fb22fb305632d7ec046eb76efa4ff3efc9594
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-21 15:31:08 +0100

    feat: redirect to home page on click of the navbar header

--- frontend/src/components/shared/Navigation/TopNavigationBar.vue
+++ frontend/src/components/shared/Navigation/TopNavigationBar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="flex top-0 z-50 w-full bg-red-900 py-4 pl-10">
-        <span class="text-white text-2xl font-bold">{{ t('nav-bar.title') }}</span>
+        <router-link to="/">
+            <span class="text-white text-2xl font-bold cursor-pointer">{{ t('nav-bar.title') }}</span>
+        </router-link>
     </div>
 </template>
 
