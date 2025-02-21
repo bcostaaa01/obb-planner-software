ecf1ed2b37b1f6fd2cd20637f09c5939dd5d63b4
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-21 15:27:36 +0100

    feat: add translations for trip planner

--- frontend/src/components/TripPlanner/TripPlannerForm.vue
+++ frontend/src/components/TripPlanner/TripPlannerForm.vue
@@ -1,16 +1,16 @@
 <template>
     <fwb-card class="flex flex-col p-5 mx-4">
         <div class="flex flex-col mb-10">
-            <fwb-select id="abfahrt" label="Abfahrt" model-value="" required size="sm" type="multiselect" class="h-10"
-                placeholder="Wähle eine Abfahrtsstation" :options="stations" />
+            <fwb-select id="abfahrt" :label="t('trip-planner.departure')" model-value="" required size="sm"
+                type="multiselect" class="h-10" placeholder="Wähle eine Abfahrtsstation" :options="stations" />
         </div>
         <div class="flex flex-col mb-10">
-            <fwb-select id="ankunft" label="Ankunft" model-value="" required size="sm" type="multiselect" class="h-10"
-                placeholder="Wähle eine Ankunftsstation" :options="stations" />
+            <fwb-select id="ankunft" :label="t('trip-planner.destination')" model-value="" required size="sm"
+                type="multiselect" class="h-10" placeholder="Wähle eine Ankunftsstation" :options="stations" />
         </div>
 
         <div class="flex flex-col">
-            <fwb-button type="submit" size="sm" class="cursor-pointer">Suchen</fwb-button>
+            <fwb-button type="submit" size="sm" class="cursor-pointer">{{ t('trip-planner.search') }}</fwb-button>
         </div>
     </fwb-card>
 </template>
@@ -18,6 +18,9 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { FwbButton, FwbSelect, FwbCard } from 'flowbite-vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const stations = ref([{
     name: "Wien Hbf",
