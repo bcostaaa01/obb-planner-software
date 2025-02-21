e8aed15bf6259b14b96fe422eaf66c114afba6f6
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-21 15:25:28 +0100

    feat: basis for trip planner form

---
+++ frontend/src/components/TripPlanner/TripPlannerForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <fwb-card class="flex flex-col p-5 mx-4">
+        <div class="flex flex-col mb-10">
+            <fwb-select id="abfahrt" label="Abfahrt" model-value="" required size="sm" type="multiselect" class="h-10"
+                placeholder="Wähle eine Abfahrtsstation" :options="stations" />
+        </div>
+        <div class="flex flex-col mb-10">
+            <fwb-select id="ankunft" label="Ankunft" model-value="" required size="sm" type="multiselect" class="h-10"
+                placeholder="Wähle eine Ankunftsstation" :options="stations" />
+        </div>
+
+        <div class="flex flex-col">
+            <fwb-button type="submit" size="sm" class="cursor-pointer">Suchen</fwb-button>
+        </div>
+    </fwb-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { FwbButton, FwbSelect, FwbCard } from 'flowbite-vue';
+
+const stations = ref([{
+    name: "Wien Hbf",
+    value: "Wien Hbf"
+},
+{
+    name: "Wien Meidling",
+    value: "Wien Meidling"
+},
+{
+    name: "Wien Westbahnhof",
+    value: "Wien Westbahnhof"
+},
+{
+    name: "Wien Mitte",
+    value: "Wien Mitte"
+},
+])
+</script>
---
+++ frontend/src/components/shared/Form/InputField.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col">
+        <fwb-input :id="id" :label="label" :model-value="modelValue" :required="required" type="text" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { FwbInput } from 'flowbite-vue';
+
+defineProps<{
+    id: string;
+    label: string;
+    modelValue: string;
+    required: boolean;
+    size: string;
+}>();
+</script>
