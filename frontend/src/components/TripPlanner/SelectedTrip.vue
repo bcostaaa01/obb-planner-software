64a130727f90a66448bb5d1f2ef5f8712a96c560
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-24 21:18:16 +0100

    feat: allow change train direction

--- frontend/src/components/TripPlanner/SelectedTrip.vue
+++ frontend/src/components/TripPlanner/SelectedTrip.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="flex flex-col bg-white shadow-md p-4 w-full">
         <div class="flex justify-between items-center mb-4">
-            <h2 class="text-xl font-bold text-gray-900">{{ departure }} › {{ destination }}</h2>
+            <h2 class="text-xl font-bold text-gray-900">{{ currentDeparture }} › {{ currentDestination }}</h2>
             <div class="flex items-center">
                 <input type="checkbox" class="mr-2" v-model="bestFareChecked" />
-                <span class="text-sm">{{ t('trip-planner.best-fare') }}</span>
+                <span class="text-sm">{{ checkboxLabel }}</span>
             </div>
         </div>
         <div class="flex justify-between items-center mb-4">
             <div class="flex items-center">
-                <span class="text-sm">{{ t('trip-planner.direction') }}</span>
+                <button class="text-sm flex items-center cursor-pointer" @click="toggleDirection">
+                    <FontAwesomeIcon :icon="faArrowsTurnToDots" class="mr-2" />
+                    {{ t('trip-planner.direction') }}
+                </button>
                 <svg class="w-4 h-4 mx-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                 </svg>
@@ -22,19 +25,13 @@
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faArrowsTurnToDots } from '@fortawesome/free-solid-svg-icons';
 
 const { t } = useI18n();
 
-defineProps({
-    departure: {
-        type: String,
-        required: true
-    },
-    destination: {
-        type: String,
-        required: true
-    },
+const props = defineProps({
     date: {
         type: String,
         required: true
@@ -43,11 +40,27 @@ defineProps({
         type: String,
         required: true
     },
-    title: {
+    departure: {
         type: String,
         required: true
     },
+    destination: {
+        type: String,
+        required: true
+    },
+    checkboxLabel: {
+        type: String,
+        default: "Beste Fahrkarte"
+    }
 });
 
 const bestFareChecked = ref(false);
+const isReversed = ref(false);
+
+const currentDeparture = computed(() => isReversed.value ? props.destination : props.departure);
+const currentDestination = computed(() => isReversed.value ? props.departure : props.destination);
+
+function toggleDirection() {
+    isReversed.value = !isReversed.value;
+}
 </script>
