c44633bad83b2c8dc1cdbaa48042e2cb9a6ef4fe
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-24 21:35:16 +0100

    feat: improve UI of the logo in the top nav bar

--- frontend/src/components/shared/Navigation/TopNavigationBar.vue
+++ frontend/src/components/shared/Navigation/TopNavigationBar.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="flex top-0 z-50 w-full py-4 pl-10 bg-gray-100 border-b border-gray-200">
+    <div class="flex top-0 z-50 w-full py-4 pl-5 bg-gray-100 border-b border-gray-200">
         <router-link to="/" class="flex items-center gap-2">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Logo_%C3%96BB.svg/1200px-Logo_%C3%96BB.svg.png"
-                class="h-10" />
-            <span class="text-gray-800 text-2xl font-bold cursor-pointer">{{ t('nav-bar.title') }}</span>
+                class="h-6" />
         </router-link>
     </div>
 </template>
