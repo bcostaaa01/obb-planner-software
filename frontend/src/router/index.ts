d7028cf650c422e3b16b266fe73265acddd09792
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-02-27 18:24:50 +0100

    feat: check for path matches and define 404 page, add translations

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,17 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import TripDetails from "../views/TripDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
+      name: "home",
       component: Home,
     },
     {
       path: "/trip-details",
+      name: "trip-details",
       component: TripDetails,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      component: NotFound,
     },
   ],
 });
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <AppLayout>
+        <div class="flex flex-col items-center justify-center h-screen">
+            <h1 class="text-4xl font-bold">{{ t('not-found.title') }}</h1>
+            <div class="flex flex-col items-center justify-center">
+                <p class="text-2xl mt-4">{{ t('not-found.message') }}</p>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script setup lang="ts">
+import AppLayout from '../components/layouts/AppLayout.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
