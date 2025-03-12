3ddff2e161f7a58e8d42f8ea7147f95b79960754
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-03-12 23:35:38 +0100

    feat: add select for the payment method (WIP)

--- frontend/src/components/Checkout/PaymentMethod.vue
+++ frontend/src/components/Checkout/PaymentMethod.vue
@@ -1,24 +1,30 @@
 <template>
     <CheckoutStepLayout title="Payment Method">
-        <div class="flex flex-col items-center mt-4">
-            <div class="flex space-x-8">
+        <div class="flex flex-col items-center mt-4 w-full">
+            <div class="flex flex-col space-y-4 w-full">
                 <div v-for="(method, index) in paymentMethods" :key="index"
-                    class="flex flex-col items-center cursor-pointer" @click="selectPaymentMethod(index)">
-                    <div :class="classes(index)"
-                        class="flex items-center justify-center rounded-lg p-1 transition duration-200">
-                        <img :src="method.icon" :alt="method.name" class="w-12" />
-                    </div>
-                    <span class="mt-2 text-sm text-gray-600">{{ method.name }}</span>
+                    class="flex items-center hover:bg-indigo-50 transition duration-200"
+                    @click="selectPaymentMethod(method.name)">
+                    <input type="radio" :value="method.name" v-model="selectedMethod" class="hidden peer" />
+                    <label :for="method.name" :class="classes(method.name)"
+                        class="flex items-center justify-between w-full p-3 border rounded-md transition duration-200 peer-checked:bg-indigo-100 peer-checked:text-indigo-900 peer-checked:ring-2 peer-checked:ring-indigo-200 cursor-pointer">
+                        <div class="flex items-center">
+                            <img :src="method.icon" :alt="method.name" class="w-8 mr-2" />
+                            <span class="text-sm">{{ method.name }}</span>
+                        </div>
+                        <div v-if="selectedMethod === method.name" class="w-4 h-4 rounded-full bg-indigo-500"></div>
+                    </label>
                 </div>
             </div>
         </div>
-        <div class="flex flex-col mt-4">
+        <!-- <div class="flex flex-col mt-4">
             <form class="flex flex-col gap-2">
                 <div v-for="field in paymentMethodConfig" :key="field.id" class="flex flex-row gap-4 items-center">
                     <label :for="field.id" class="text-sm font-medium self-center">{{ t(field.labelKey) }}</label>
                     <input :type="field.type" :id="field.id" class="w-full p-2 border border-gray-300 rounded-md"
                         :required="field.required" />
                 </div>
+
                 <button type="submit" @click="handleSubmit"
                     class="mt-4 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-200 w-64">
                     <span v-if="loading">
@@ -27,7 +33,7 @@
                     <span v-else>{{ t('checkout.next') }}</span>
                 </button>
             </form>
-        </div>
+        </div> -->
     </CheckoutStepLayout>
 </template>
 
@@ -42,21 +48,22 @@ import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 
 const paymentMethods = [
-    { name: 'iDeal', icon: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/IDEAL_%28Bezahlsystem%29_logo.svg' },
-    { name: 'MasterCard', icon: 'https://www.designenlassen.de/blog/wp-content/uploads/2024/02/Dein-Abschnittstext-35.png' },
+    { name: 'Google Pay', icon: 'https://brandlogos.net/wp-content/uploads/2021/10/google-pay-logo.png' },
+    { name: 'Apple Pay', icon: 'https://download.logo.wine/logo/Apple_Pay/Apple_Pay-Logo.wine.png' },
+    { name: 'Credit Card', icon: 'https://www.designenlassen.de/blog/wp-content/uploads/2024/03/visa.png' },
 ];
 
 const loading = ref(false);
-const selectedMethod = ref(0);
+const selectedMethod = ref('');
 
-const selectPaymentMethod = (index: number) => {
-    selectedMethod.value = index;
+const selectPaymentMethod = (name: string) => {
+    selectedMethod.value = name;
 };
 
-const classes = (index: number) => {
+const classes = (name: string) => {
     return {
-        'border-2 border-blue-500': selectedMethod.value === index,
-        'border-gray-300': selectedMethod.value !== index
+        'border-2 border-purple-300 bg-indigo-100 text-indigo-900 ring-purple-300': selectedMethod.value === name,
+        'border-gray-300': selectedMethod.value !== name
     }
 }
 
