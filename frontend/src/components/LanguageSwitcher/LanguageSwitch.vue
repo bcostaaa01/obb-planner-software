6dbb6a50e4edd0a2942086d66b61ad1254c9e1ff
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-03-17 22:20:17 +0100

    feat: add styles for current locale

--- frontend/src/components/LanguageSwitcher/LanguageSwitch.vue
+++ frontend/src/components/LanguageSwitcher/LanguageSwitch.vue
@@ -2,7 +2,7 @@
     <div class="relative inline-block text-left">
         <div>
             <button type="button"
-                class="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 hover:bg-gray-50"
+                class="inline-flex justify-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 hover:bg-gray-50"
                 id="menu-button" aria-expanded="true" aria-haspopup="true" @click="toggleDropdown">
                 <div class="flex items-center">
                     <img :src="currentLanguage.icon" alt="Language Icon" class="rounded-sm w-5 h-4 mr-3">
@@ -13,14 +13,17 @@
         </div>
 
         <div v-if="dropdownOpen"
-            class="absolute right-0 z-10 mt-2 w-56 rounded-md bg-white ring-1 shadow-lg ring-black/5" role="menu"
+            class="absolute right-0 z-10 mt-2 w-56 bg-white ring-1 shadow-lg ring-black/5" role="menu"
             aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
                 <a v-for="language in languages" :key="language.code" href="#"
                     @click.prevent="changeLanguage(language.code)"
-                    class="flex items-center px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">
+                    :class="['flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-red-700', { 'bg-red-600': language.code === currentLanguage.code }]"
+                    role="menuitem" tabindex="-1">
                     <img :src="language.icon" alt="Language Icon" class="rounded-sm w-5 h-4 mr-2">
-                    {{ language.label }}
+                    <div :class="['text-gray-700', { 'text-white': language.code === currentLanguage.code }]">
+                        {{ language.label }}
+                    </div>
                 </a>
             </div>
         </div>
@@ -28,9 +31,18 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { useLanguageSwitch } from '../../composables/useLanguageSwitch';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const { languages, currentLanguage, dropdownOpen, toggleDropdown, changeLanguage } = useLanguageSwitch();
+
+onMounted(() => {
+    const languageParams = new URLSearchParams(window.location.search);
+    const language = languageParams.get('language');
+    if (language) {
+        changeLanguage(language);
+    }
+});
 </script>
