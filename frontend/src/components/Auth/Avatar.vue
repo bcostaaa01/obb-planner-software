cb079bb3cbba0d8c24e60551d8f817f12d625774
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-03-25 21:38:55 +0100

    feat: show login when user is not logged in instead of avatar

--- frontend/src/components/Auth/Avatar.vue
+++ frontend/src/components/Auth/Avatar.vue
@@ -1,7 +1,12 @@
 <template>
-    <span
+    <span v-if="isUserLogged"
         class="text-white text-md text-center bg-gray-900 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer mr-2"
         @click="toggleDropdown">{{ avatar }}</span>
+    <span v-else
+        class="text-gray-700 hover:text-gray-900 transition-colors hover:bg-gray-200 py-2 px-4 text-md text-center cursor-pointer mr-2"
+        @click="toggleDropdown">
+        {{ t('auth.login') }}
+    </span>
 
     <AuthLayout :show-dropdown="showDropdown" class="mt-14" :is-user-logged="isUserLogged" />
 </template>
@@ -10,6 +15,9 @@
 import { ref, onMounted } from "vue";
 import AuthLayout from "../layouts/AuthLayout.vue";
 import { useUserAvatar } from "../../composables/useUserAvatar";
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const showDropdown = ref(false);
 const isUserLogged = ref(false);
